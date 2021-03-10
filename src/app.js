ce1e6a78a0e131e11bd9ed436f2a3ce520866c98
PedroNeves <pneves355@gmail.com>
2021-03-10 11:55:15 +0000

    added all stylings before polyfill

--- src/app.js
+++ src/app.js
@@ -10,6 +10,7 @@ import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 import 'react-dates/lib/css/_datepicker.css';
 import {firebase} from './firebase/firebase';
+import LoadingPage from './components/LoadingPage';
 
 
 
@@ -29,7 +30,7 @@ const renderApp = () => {
   }
 }
 
-ReactDOM.render(<p>Loading...</p>, document.getElementById('app'));
+ReactDOM.render(<LoadingPage />, document.getElementById('app'));
 
 // Authenticated to non-auth and vice-versa . onAuthStateChanged runs a function everytime auth state changes (loggin in or out), including when we first load the application
 
